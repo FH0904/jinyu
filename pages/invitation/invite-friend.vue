32129984237322a45194a5faa674a3a605c26067
zzj <384464439@qq.com>
2021-09-03 18:40:53 +0800

    0903

---
+++ pages/invitation/invite-friend.vue
@@ -0,0 +1,82 @@
+<template>
+	<view>
+		<view class="top-header">
+			<u-navbar  :background="background" title="邀请好友" title-color="#fff" title-bold :border-bottom="false" back-icon-color="#fff" ></u-navbar>
+		</view>
+		<view class="t-code">
+			<view class="c-title">— 您的邀请码 —</view>
+			<view class="c-code">17603080910</view>
+		</view>
+		<view class="qr-code">
+			
+		</view>
+		<view class="qr-text">微信扫一扫</view>
+		<view class="qr-line"></view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				background:{
+					background: 'transparent'
+				},
+			};
+		}
+	}
+</script>
+
+<style lang="less">
+page {
+	background-color: #FFFFFF;
+}
+.top-header {
+	width: 750rpx;
+	height: 500rpx;
+	background: url(/static/images/invitation/background2@2x.png);
+	background-size: 100% 100%;
+}
+.t-code {
+	width: 670rpx;
+	height: 240rpx;
+	background: #FFFFFF;
+	box-shadow: 0px 0px 20rpx 0px rgba(0, 87, 228, 0.2);
+	border-radius: 20rpx;
+	margin: -130rpx auto 0;
+	text-align: center;
+	overflow: hidden;
+	.c-title {
+		font-size: 32rpx;
+		font-weight: 500;
+		color: #222222;
+		margin-top: 62rpx;
+	}
+	.c-code {
+		font-size: 60rpx;
+		font-weight: bold;
+		color: #2878FF;
+		margin-top: 40rpx;
+	}
+}
+.qr-code {
+	width: 280rpx;
+	height: 280rpx;
+	background: #FFFFFF;
+	border: 8rpx solid #2878FF;
+	border-radius: 20rpx;
+	margin: 60rpx auto 0;
+}
+.qr-text {
+	text-align: center;
+	margin-top: 30rpx;
+	font-size: 32rpx;
+	color: #222222;
+}
+.qr-line {
+	width: 690rpx;
+	height: 2rpx;
+	background: #F0F5FC;
+	margin: 40rpx auto 0;
+}
+</style>
--- pages/tabbar/invitation.vue
+++ pages/tabbar/invitation.vue
@@ -1,6 +1,36 @@
 <template>
-	<view>
-		
+	<view style="font-size: 0;">
+		<u-image width="100%" height="1200" src="/static/images/invitation/background@2x.png"></u-image>
+		<view class="i-bottom">
+			<view class="b-box1">
+				<view class="b-bar">邀请谁</view>
+				<view class="b-content">有 <text style="color: #0D73F2;font-weight: bold;margin: 0 10rpx;">2张以上</text> 信用卡的人</view>
+			</view>
+			<view class="b-box2">
+				<view class="b-bar">怎么奖励?</view>
+				<view class="b-list">
+					<view class="l-item">
+						<text class="i-text1">[直接邀请人]</text>
+						<text class="i-text2">每划卡1万元，奖励您3元</text>
+					</view>
+					<view class="l-item">
+						<text class="i-text1">[间接邀请人]</text>
+						<text class="i-text2">每划卡1万元，奖励您2元</text>
+					</view>
+					<view class="l-example">
+						<view class="e-title">[例]</view>
+						<view class="e-list">马云邀请了马化腾，马化腾邀请了刘强东；</view>
+						<view class="e-list">马化腾划卡1万元，马云收到奖励3元；</view>
+						<view class="e-list">刘强东划卡1万元，马化腾收到奖励3元，马云收到奖励2元。</view>
+					</view>
+				</view>
+			</view>
+			<view class="i-tip">最终解释权归金鱼所有</view>
+			<view class="invitation-btn" >
+				<u-button :custom-style="customStyle" shape="circle" :ripple="true" @click="navTo('/pages/invitation/invite-friend')">邀请</u-button>
+			</view> 
+			<view class="tabbar"></view>
+		</view>
 	</view>
 </template>
 
@@ -8,12 +38,119 @@
 	export default {
 		data() {
 			return {
-				
+				customStyle:{
+					width: '690rpx',
+					height: '80rpx',
+					background: 'linear-gradient(0deg, #1B91FF 0%, #0056E4 100%)',
+					borderRadius: '40rpx',
+					color:'#fff'
+				}
 			};
+		},
+		methods: {
+			navTo(url) {
+				uni.navigateTo({
+					url
+				})
+			}
 		}
 	}
 </script>
 
 <style lang="less">
-
+page {
+	background-color: #1f158f;
+}
+.i-bottom {
+	.b-box1 {
+		position: relative;
+		width: 690rpx;
+		height: 240rpx;
+		text-align: center;
+		line-height: 260rpx;
+		background: #FFFFFF;
+		border-radius: 20rpx;
+		margin: -360rpx auto 0;
+		.b-bar {
+			position: absolute;
+			left: 50%;
+			top: -20rpx;
+			transform: translateX(-50%);
+			width: 470rpx;
+			height: 80rpx;
+			line-height: 80rpx;
+			text-align: center;
+			font-weight: bold;
+			background: url(../../static/images/invitation/title@2x.png);
+			background-size: 100% 100%;
+			color: #FFFFFF;
+			font-size: 36rpx;
+		}
+		.b-content {
+			font-size: 32rpx;
+		}
+	}
+	.b-box2 {
+		position: relative;
+		width: 690rpx;
+		height: 520rpx;
+		background: #FFFFFF;
+		border-radius: 20rpx;
+		margin: 40rpx auto 0;
+		padding: 60rpx 56rpx 0 40rpx;
+		.b-bar {
+			position: absolute;
+			left: 50%;
+			top: -20rpx;
+			transform: translateX(-50%);
+			width: 470rpx;
+			height: 80rpx;
+			line-height: 80rpx;
+			text-align: center;
+			font-weight: bold;
+			background: url(../../static/images/invitation/title@2x.png);
+			background-size: 100% 100%;
+			color: #FFFFFF;
+			font-size: 36rpx;
+		}
+		.b-list {
+			font-size: 28rpx;
+			.l-item {
+				margin-top: 40rpx;
+				.i-text1 {
+					font-weight: bold;
+					color: #0D72F1;
+					margin-right: 16rpx;
+				}
+			}
+			.l-example {
+				margin-top: 40rpx;
+				.e-title {
+					font-weight: bold;
+					color: #0D72F1;
+				}
+				.e-list {
+					margin-top: 20rpx;
+					font-size: 28rpx;
+					line-height: 40rpx;
+				}
+			}
+		}
+	}
+	.i-tip {
+		text-align: center;
+		margin: 40rpx 0;
+		font-size: 24rpx;
+		color: #AAAAAA;
+	}
+	.invitation-btn {
+		width: 750rpx;
+		height: 120rpx;
+		background: #FFFFFF;
+		padding: 0 30rpx;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	}
+}
 </style>
